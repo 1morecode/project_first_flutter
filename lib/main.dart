abe8e60474e8ef1da98e53d887bd67f0756917e7
1morecode <1morecode@gmail.com>
2024-04-28 20:27:45 +0530

    Title Changed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "My First Flutter App",
+      title: "My First App",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
